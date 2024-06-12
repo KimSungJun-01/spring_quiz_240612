9d3955bb9b842360c9c4f44295e51a5500c0565d
DESKTOP-92INLTK\user <KimSungJun-01>
2024-06-12 17:27:46 +0900

    초기환경 설

---
+++ src/main/java/com/quiz/lesson01/User.java
@@ -0,0 +1,32 @@
+package com.quiz.lesson01;
+
+public class User {
+	// field
+	private String title;
+	private String user;
+	private String content;
+	
+	// getter
+	public String getTitle() {
+		return title;
+	}
+	public String getUser() {
+		return user;
+	}
+	public String getContent() {
+		return content;
+	}
+	
+	// setter
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
